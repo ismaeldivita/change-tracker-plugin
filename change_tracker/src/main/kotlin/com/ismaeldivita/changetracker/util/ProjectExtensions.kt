3cbbe0f61dcf23b4d1684ec264586a49847eef4b
ismaeldivita <ismaeldivita@gmail.com>
2020-07-02 15:59:12 +0100

    Avoid crash for not existent extension

--- change_tracker/src/main/kotlin/com/ismaeldivita/changetracker/util/ProjectExtensions.kt
+++ change_tracker/src/main/kotlin/com/ismaeldivita/changetracker/util/ProjectExtensions.kt
@@ -2,11 +2,16 @@ package com.ismaeldivita.changetracker.util
 
 import com.ismaeldivita.changetracker.ChangeTrackerExtension
 import org.gradle.api.Project
+import java.lang.Exception
 
 val Project.isRoot get() = this == rootProject
 
 @Suppress("UNCHECKED_CAST")
-fun <T> Project.getExtraProperty(key: String): T? = extensions.extraProperties[key] as T?
+fun <T> Project.getExtraProperty(key: String): T? = try {
+    extensions.extraProperties[key] as T?
+} catch (error: Exception) {
+    null
+}
 
 val Project.changeTrackerExtension: ChangeTrackerExtension
     get() = extensions.getByName(CHANGE_TRACKER_EXTENSION) as ChangeTrackerExtension
